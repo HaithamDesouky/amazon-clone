fc8d58103e66844a092bc43b9a62c4480d37e767
HaithamDesouky <haithamadesouky@gmail.com>
2020-11-26 16:27:50 +0100

    before authentication

---
+++ src/app/COMPONENTS/header/header.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+import { ShoppingCartService } from 'src/app/SERVICES/shopping-cart.service';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
+})
+export class HeaderComponent implements OnInit {
+  constructor(public shoppingCart: ShoppingCartService) {}
+
+  ngOnInit(): void {}
+}
