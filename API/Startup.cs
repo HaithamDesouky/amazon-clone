48d869c046cc39e8e0028a3e91c20b9e186fc61d
HaithamDesouky <haithamadesouky@gmail.com>
2020-12-07 11:28:08 +0100

    redeploy

--- API/Startup.cs
+++ API/Startup.cs
@@ -37,48 +37,7 @@ namespace API
     public void ConfigureServices(IServiceCollection services)
     {
 
-          services.AddScoped<IProductRepository, ProductRepository>();
-          services.AddScoped<IOrderService, OrderRepository>();
-                    services.AddAutoMapper(typeof(AutoMapperProfiles).Assembly);
-
-        
-
-    services.AddDbContext<DataContext>(options =>
-{
-    var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
-
-    string connStr;
-
-    // Depending on if in development or production, use either Heroku-provided
-    // connection string, or development connection string from env var.
-    if (env == "Development")
-    {
-        // Use connection string from file.
-        connStr = _config.GetConnectionString("DefaultConnection");
-    }
-    else
-    {
-        // Use connection string provided at runtime by Heroku.
-        var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
-
-        // Parse connection URL to connection string for Npgsql
-        connUrl = connUrl.Replace("postgres://", string.Empty);
-        var pgUserPass = connUrl.Split("@")[0];
-        var pgHostPortDb = connUrl.Split("@")[1];
-        var pgHostPort = pgHostPortDb.Split("/")[0];
-        var pgDb = pgHostPortDb.Split("/")[1];
-        var pgUser = pgUserPass.Split(":")[0];
-        var pgPass = pgUserPass.Split(":")[1];
-        var pgHost = pgHostPort.Split(":")[0];
-        var pgPort = pgHostPort.Split(":")[1];
-
-        connStr = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb}";
-    }
-
-    // Whether the connection string came from the local development configuration file
-    // or from the environment variable from Heroku, use it to set up your DbContext.
-    options.UseNpgsql(connStr);
-});
+        services.AddApplicationServices(_config);
         services.AddControllers();
         services.AddCors();
         
@@ -90,22 +49,26 @@ namespace API
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
-      // if (env.IsDevelopment())
-      // {
-      //   app.UseDeveloperExceptionPage();
-      // }
+ 
 
+      // app.UseStatusCodePagesWithReExecute("/errors/{0}");
 
       app.UseHttpsRedirection();
 
       app.UseRouting();
 
-      app.UseCors(x => x.AllowAnyHeader()
-            .AllowAnyMethod()
-            .AllowCredentials()
-            .WithOrigins("http://localhost:4200"));
+       app.UseCors(x => x.AllowAnyHeader()
+                .AllowAnyMethod()
+                .AllowCredentials()
+                .WithOrigins("https://localhost:4200"));
 
-          
+
+            
+
+      // app.UseCors(x => x.AllowAnyHeader()
+      //             .AllowAnyMethod()
+      //             .WithOrigins("http://localhost:4200"));
+
 
       app.UseDefaultFiles();
       app.UseStaticFiles();
