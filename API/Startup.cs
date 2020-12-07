cc348b687d99f00aebb0041ec822854081b8f1db
HaithamDesouky <haithamadesouky@gmail.com>
2020-12-07 13:28:06 +0100

    groundhog

--- API/Startup.cs
+++ API/Startup.cs
@@ -48,7 +48,7 @@ namespace API
        app.UseCors(x => x.AllowAnyHeader()
                 .AllowAnyMethod()
                 .AllowCredentials()
-                .WithOrigins("https://localhost:4200"));
+                .WithOrigins("http://localhost:4200"));
 
 
             
